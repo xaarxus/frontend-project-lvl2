8475e7708bf71cee68443a80709fed071aca284c
newsalaar <keyshet@gmail.com>
2021-01-21 18:58:41 +0100

    update simple test

--- __tests__/logic-gendiff.test.js
+++ __tests__/logic-gendiff.test.js
@@ -1,9 +1,16 @@
 import { test, expect } from '@jest/globals';
+import { fileURLToPath } from 'url';
+import * as path from 'path';
 import genDiff from '../src/logic-gendiff.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '__fixtures__', filename);
+
 test('simple test genDiff', () => {
-  const filepath1 = '__tests__/__fixtures__/file1.json';
-  const filepath2 = '__tests__/__fixtures__/file2.json';
+  const filepath1 = getFixturePath('file1.json');
+  const filepath2 = getFixturePath('file2.json');
   const expectValue = '{\n    host: hexlet.io\n  - timeout: 50\n  + timeout: 20\n  - proxy: 123.234.53.22\n  - follow: false\n  + verbose: true\n}';
 
   expect(genDiff(filepath1, filepath2)).toEqual(expectValue);
