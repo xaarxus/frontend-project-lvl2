b32122cdf1c42988bd6bbc743d9a44dc6ba1ca72
newsalaar <keyshet@gmail.com>
2021-01-30 15:59:09 +0100

    fix plain formatter

--- formatters/plain.js
+++ formatters/plain.js
@@ -1,66 +1,53 @@
-import _ from 'lodash';
+import creatorASTtree from '../src/creatorASTtree.js';
 
 const plain = (data1, data2) => {
-  const mergeObject = _.merge({}, data1, data2);
+  const astTree = creatorASTtree(data1, data2);
 
-  const searchDiffInObject = (obj, path = []) => {
-    const keys = _.keys(obj).sort();
+  const isObject = (obj) => typeof obj === 'object' && obj !== null;
 
-    const toString = keys.reduce((acc, key) => {
-      const newPath = [...path, key];
-      const first = _.get(data1, newPath, undefined);
-      const second = _.get(data2, newPath, undefined);
-
-      if (second === undefined) {
-        const str = `Property '${newPath.join('.')}' was removed`;
-        const newAcc = [...acc, str];
-        return newAcc;
-      }
-      if (first === undefined) {
-        if (_.isObject(second)) {
-          const str = `Property '${newPath.join('.')}' was added with value: [complex value]`;
-          const newAcc = [...acc, str];
-          return newAcc;
-        }
-        const value = typeof second === 'string' ? `'${second}'` : `${second}`;
-        const str = `Property '${newPath.join('.')}' was added with value: ${value}`;
-        const newAcc = [...acc, str];
-        return newAcc;
-      }
-      if (_.isObject(obj[key])) {
-        const str = searchDiffInObject(obj[key], newPath);
-        const newAcc = [...acc, ...str];
-        return newAcc;
-      }
-      if (second !== undefined && first !== undefined) {
-        if (first === second) {
-          return acc;
-        }
-        if (_.isObject(second)) {
-          const value = typeof first === 'string' ? `'${first}'` : `${first}`;
-          const str = `Property '${newPath.join('.')}' was updated. From ${value} to [complex value]`;
-          const newAcc = [...acc, str];
-          return newAcc;
-        }
-        if (_.isObject(first)) {
-          const value = typeof second === 'string' ? `'${second}'` : `${second}`;
-          const str = `Property '${newPath.join('.')}' was updated. From [complex value] to ${value}`;
-          const newAcc = [...acc, str];
-          return newAcc;
-        }
-        const value1 = typeof first === 'string' ? `'${first}'` : `${first}`;
-        const value2 = typeof second === 'string' ? `'${second}'` : `${second}`;
-        const str = `Property '${newPath.join('.')}' was updated. From ${value1} to ${value2}`;
-        const newAcc = [...acc, str];
-        return newAcc;
-      }
-      return acc;
-    }, []);
-
-    return toString;
+  const normalizeValur = (value) => {
+    if (value === 'nested') {
+      return '[complex value]';
+    }
+    if (typeof value === 'string') {
+      return `'${value}'`;
+    }
+    return value;
   };
 
-  const result = searchDiffInObject(mergeObject).join('\n');
+  const toString = (arr, path = []) => arr.reduce((acc, item) => {
+    const {
+      name,
+      value,
+      status,
+      children = undefined,
+      oldValue = undefined,
+    } = item;
+    if (status === 'outdated') {
+      if (children !== undefined) {
+        return [...acc, toString(children, [...path, name])];
+      }
+      return acc;
+    }
+    if (status === 'added') {
+      const newValue = normalizeValur(value);
+      const str = `Property '${[...path, name].join('.')}' was added with value: ${newValue}`;
+      acc.push(str);
+      return acc;
+    }
+    if (status === 'updated') {
+      const newValue = normalizeValur(value);
+      const previousValue = isObject(oldValue) ? '[complex value]' : normalizeValur(oldValue);
+      const str = `Property '${[...path, name].join('.')}' was updated. From ${previousValue} to ${newValue}`;
+      acc.push(str);
+      return acc;
+    }
+    const str = `Property '${[...path, name].join('.')}' was removed`;
+    acc.push(str);
+    return acc;
+  }, []).flat();
+
+  const result = toString(astTree).join('\n');
   return result;
 };
 
